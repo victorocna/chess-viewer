fa0fd2c5eb83777506b6f4be0e94063b5e42a6d4
victorocna <victorocnarescu@gmail.com>
2020-08-28 13:18:52 +0300

    update move number logic

--- src/functions/getMoveNumber.js
+++ src/functions/getMoveNumber.js
@@ -1,13 +1,7 @@
 export const getMoveNumber = (fen) => {
-  let moveNumber = 0;
+  const split = fen.split(' ');
+  const side = split[1];
+  const fullmove = split[5];
 
-  for(let i = fen.length - 1; fen[i] !== " "; i--) {
-    moveNumber += fen[i] * Math.pow(10, fen.length - 1 - i);
-  }
-
-  if (fen.includes(' w ')) {
-    moveNumber--;
-  }
-
-  return moveNumber;
+  return side === 'w' ? fullmove - 1 : fullmove;
 };
